0985a20a209337e348bb4476c33422cf87fbdae4
2y2son4 <adrianadelteso@gmail.com>
2021-04-07 11:07:58 +0200

    :rocket: Update day 03 files

--- 03-css-variables/js/main.js
+++ 03-css-variables/js/main.js
@@ -14,7 +14,7 @@ function handleUpdate() {
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
-inputs.forEach((input) => input.addEventListener('change', handleUpdate));
+// inputs.forEach((input) => input.addEventListener('change', handleUpdate));
 inputs.forEach((input) => input.addEventListener('mousemove', handleUpdate));
 
 function handleReset() {
