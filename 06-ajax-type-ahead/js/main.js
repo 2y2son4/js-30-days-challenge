fa60ab6726f18f96999c1a87723876e5e62debad
2y2son4 <adrianadelteso@gmail.com>
2021-04-11 17:46:39 +0200

    .sparkles: Add main script

--- 06-ajax-type-ahead/js/main.js
+++ 06-ajax-type-ahead/js/main.js
@@ -4,6 +4,8 @@ const endpoint =
   'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
 const cities = [];
+const inputSearch = document.querySelector('.search');
+const resultSearch = document.querySelector('.suggestions');
 
 fetch(endpoint)
   .then((data) => data.json())
@@ -17,6 +19,38 @@ function findMatches(search, cities) {
 }
 
 function displayMatches() {
-  console.log(this.value);
+  const matchArray = findMatches(this.value, cities);
+  const paintResults = matchArray
+    .map((place) => {
+      const regex = new RegExp(this.value, 'gi');
+      const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+      const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+      function population(number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+      }
+
+      //   console.log(place);
+      //
+      function icon(number) {
+        if (number < 30000) {
+          return (icon = '🏡');
+        } else {
+          return (icon = '🏙️');
+        }
+      }
+      return `
+      <li>
+      <span class="name"> ${icon(place.population)} ${cityName}, ${stateName}</span>
+      <span class="name"> ${population(place.population)} 👩‍🦲</span>
+      <span class="population"> ${place.latitude},</span>
+      <span class="population"> ${place.longitude}</span>
+      </li>
+      `;
+    })
+    .join('');
+
+  resultSearch.innerHTML = paintResults;
 }
-console.log(cities);
+
+inputSearch.addEventListener('change', displayMatches);
+inputSearch.addEventListener('keyup', displayMatches);
