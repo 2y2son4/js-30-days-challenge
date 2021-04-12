e46e69563f693f9611f821f65a9a62c096ef4f12
2y2son4 <adrianadelteso@gmail.com>
2021-04-12 13:29:13 +0200

    :sparkles: Add main function to start drawing

--- 08-fun-with-HTML-canvas/js/main.js
+++ 08-fun-with-HTML-canvas/js/main.js
@@ -1,1 +1,57 @@
 'use strict';
+
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = '#fabada';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 50;
+
+// like blend mode in photoshop
+// ctx.globalCompositeOperation = 'multiply';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(ev) {
+  if (!isDrawing) return; // stop the function from running when mouse is not clicked
+  ctx.strokeStyle = `hsl(${hue}, 50%, 50%)`;
+
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  // go to
+  ctx.lineTo(ev.offsetX, ev.offsetY);
+  ctx.stroke();
+  // destructuring array
+  [lastX, lastY] = [ev.offsetX, ev.offsetY];
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 20) {
+    // flip direction
+    direction = !direction;
+  }
+
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+}
+
+canvas.addEventListener('mousedown', (ev) => {
+  isDrawing = true;
+  [lastX, lastY] = [ev.offsetX, ev.offsetY];
+});
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => (isDrawing = false));
+canvas.addEventListener('mouseout', () => (isDrawing = false));
