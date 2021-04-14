897c53953543beca6f82153f334a5f8501e929d6
2y2son4 <adrianadelteso@gmail.com>
2021-04-14 14:50:15 +0200

    :poop: Try to implement localStorage

--- 10-checkboxes/js/main.js
+++ 10-checkboxes/js/main.js
@@ -62,34 +62,6 @@ const inputsTextConfig = [
   },
 ];
 
-function getTask() {
-  return {
-    add1: document.querySelector('.add1').value,
-    add2: document.querySelector('.add2').value,
-    add3: document.querySelector('.add3').value,
-    add4: document.querySelector('.add4').value,
-    add5: document.querySelector('.add5').value,
-    add6: document.querySelector('.add6').value,
-    add7: document.querySelector('.add7').value,
-    add8: document.querySelector('.add8').value,
-    add9: document.querySelector('.add9').value,
-  };
-}
-
-function getParagraph() {
-  return {
-    task1: document.querySelector('.task1').innerHTML,
-    task2: document.querySelector('.task2').innerHTML,
-    task3: document.querySelector('.task3').innerHTML,
-    task4: document.querySelector('.task4').innerHTML,
-    task5: document.querySelector('.task5').innerHTML,
-    task6: document.querySelector('.task6').innerHTML,
-    task7: document.querySelector('.task7').innerHTML,
-    task8: document.querySelector('.task8').innerHTML,
-    task9: document.querySelector('.task9').innerHTML,
-  };
-}
-
 function updateAllInputs() {
   for (const inputTextConfig of inputsTextConfig) {
     const inputElement = document.querySelector(inputTextConfig.inputClass);
@@ -107,6 +79,7 @@ function updateAllInputs() {
       taskElement.innerHTML = newValue;
     }
   }
+  saveInLocalStorage();
 }
 
 let lastChecked;
@@ -144,4 +117,44 @@ function preventSubmit(ev) {
 }
 formElement.addEventListener('submit', preventSubmit);
 
+// Local Storage
+
+function getAllTasks() {
+  return {
+    task1: document.querySelector('.add1').value,
+    task2: document.querySelector('.add2').value,
+    task3: document.querySelector('.add3').value,
+    task4: document.querySelector('.add4').value,
+    task5: document.querySelector('.add5').value,
+    task6: document.querySelector('.add6').value,
+    task7: document.querySelector('.add7').value,
+    task8: document.querySelector('.add8').value,
+    task9: document.querySelector('.add9').value,
+  };
+}
+
+function saveInLocalStorage() {
+  const userTask = getAllTasks();
+  const userTaskInString = JSON.stringify(userTask);
+  localStorage.setItem('userTask', userTaskInString);
+}
+
+function getFromLocalStorage() {
+  const userTaskInString = localStorage.getItem('userTaskInString');
+  if (userTaskInString !== null) {
+    const userTask = JSON.parse(userTaskInString);
+    document.querySelector('.add1').value = userTask.task1;
+    document.querySelector('.add2').value = userTask.task2;
+    document.querySelector('.add3').value = userTask.task3;
+    document.querySelector('.add4').value = userTask.task4;
+    document.querySelector('.add5').value = userTask.task5;
+    document.querySelector('.add6').value = userTask.task6;
+    document.querySelector('.add7').value = userTask.task7;
+    document.querySelector('.add8').value = userTask.task8;
+    document.querySelector('.add9').value = userTask.task9;
+  }
+
+  updateAllInputs();
+}
+getFromLocalStorage();
 updateAllInputs();
