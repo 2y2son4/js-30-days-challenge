7fe70026c875b865c3ed5786ab79a3a5c9da5cd0
2y2son4 <adrianadelteso@gmail.com>
2021-04-16 14:33:07 +0200

    :sparkles: Add main functionality and :unicorn:

--- 12-key-sequence-detection/js/main.js
+++ 12-key-sequence-detection/js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const pressedKeys = [];
+const secretCode = 'aconcawa';
+window.addEventListener('keyup', (ev) => {
+  pressedKeys.push(ev.key);
+  pressedKeys.splice(-secretCode.length - 1, pressedKeys.length - secretCode.length);
+
+  if (pressedKeys.join('').includes(secretCode)) {
+    console.log("DING, DING. We've got a winner");
+    cornify_add();
+  }
+  console.log(pressedKeys);
+});
