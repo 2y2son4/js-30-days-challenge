bcfcc265973967458c36afd3ac9b13e550c95b8a
2y2son4 <adrianadelteso@gmail.com>
2021-04-16 10:15:13 +0200

    :construction: Get element from the DOM to start functions

--- 11-custom-HTML5-videoplayer/js/main.js
+++ 11-custom-HTML5-videoplayer/js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+// Get elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const perogressBar = player.querySelector('.progress__filled');
+
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
+
+// Build functions
+
+// Hook event listeners
