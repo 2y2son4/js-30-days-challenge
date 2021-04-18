1c393e000dde74ce535a14f273a3051f0dc9f005
2y2son4 <adrianadelteso@gmail.com>
2021-04-18 19:09:14 +0200

    :sparkles: Add function for images

--- 13-slide-in-on-scroll/js/main.js
+++ 13-slide-in-on-scroll/js/main.js
@@ -1,17 +1,42 @@
 'use strict';
 
+// debounce scroll function
 function debounce(func, wait = 20, immediate = true) {
-  var timeout;
+  let timeout;
   return function () {
-    var context = this,
+    let context = this,
       args = arguments;
-    var later = function () {
+    let later = function () {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
-    var callNow = immediate && !timeout;
+    let callNow = immediate && !timeout;
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
   };
 }
+
+const images = document.querySelectorAll('.slide-in');
+
+function checkSlide(ev) {
+  //   console.count(ev);
+  images.forEach((image) => {
+    // half-way through the image
+    const slideInAt = window.scrollY + window.innerHeight - image.height / 2;
+    console.log(slideInAt);
+    // bottom of the image
+    const imageBottom = image.offsetTop + image.height;
+
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrollPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrollPast) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
