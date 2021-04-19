c40fca505bb5141ff23191740a5218c804c8c110
2y2son4 <adrianadelteso@gmail.com>
2021-04-19 17:52:00 +0200

    :sparkles: Add create list function

--- 15-local-storage-and-event-delegation/js/main.js
+++ 15-local-storage-and-event-delegation/js/main.js
@@ -3,3 +3,33 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem(ev) {
+  ev.preventDefault();
+  const text = this.querySelector('[name=item]').value;
+  const item = {
+    text,
+    done: false,
+  };
+
+  // add new item to items array
+  items.push(item);
+  populateList(items, itemsList);
+  this.reset();
+}
+
+// isolate function to add plates to list
+function populateList(plates = [], platesList) {
+  let list = plates
+    .map((plate, i) => {
+      return `
+      <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}/>
+        <label for="item${i}">${plate.text}</label>
+      </li>`;
+    })
+    .join('');
+  platesList.innerHTML = list;
+}
+
+addItems.addEventListener('submit', addItem);
