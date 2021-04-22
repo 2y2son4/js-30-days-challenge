ecadfab2cfcf1f0fbc84ad1523c658b57f1d4ea8
2y2son4 <adrianadelteso@gmail.com>
2021-04-22 17:45:05 +0200

    :bug: Fix problem with new version of web navigator

--- 19-unreal-webcam/js/main.js
+++ 19-unreal-webcam/js/main.js
@@ -5,3 +5,12 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then((localMediaStream) => {
+    video.srcObject = localMediaStream;
+    video.play();
+  });
+}
+
+getVideo();
