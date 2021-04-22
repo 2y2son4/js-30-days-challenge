fee62ec3f7c9d9fd809408fa396e0821802bc929
2y2son4 <adrianadelteso@gmail.com>
2021-04-22 12:12:54 +0200

    :sparkles: Add main sort function

--- 17-sort-list/js/main.js
+++ 17-sort-list/js/main.js
@@ -15,3 +15,11 @@ const bands = [
   'Anywhere But Here',
   'An Old Dog',
 ];
+
+const strip = (bandName) => {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
+};
+
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+document.querySelector('#bands').innerHTML = sortedBands.map((band) => `<li>${band}</li>`).join('');
