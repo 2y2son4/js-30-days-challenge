0db202769cbc15b1df43fb3bf46273b053237962
2y2son4 <adrianadelteso@gmail.com>
2021-04-22 13:43:56 +0200

    :sparkles: Add function to calculate the total time of the videos

--- 18-reduce/js/main.js
+++ 18-reduce/js/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// convert the node list of times into an array
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+const seconds = timeNodes
+  .map((node) => node.dataset.time)
+  .map((timeCode) => {
+    const [mins, secs] = timeCode.split(':').map(parseFloat);
+    return mins * 60 + secs;
+  })
+  .reduce((total, vidSeconds) => total + vidSeconds);
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(
+  `The total time you'll nedd to watch the videos is ${hours} hours, ${mins} minutes and ${secondsLeft} seconds`
+);
