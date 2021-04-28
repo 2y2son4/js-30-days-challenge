6a76272882fc6093b1db4a2c0cc6d2007e98aebb
2y2son4 <adrianadelteso@gmail.com>
2021-04-28 19:58:56 +0200

    :sparkles: Add geolocation function and error

--- 21-geolocation/js/main.js
+++ 21-geolocation/js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition(
+  (data) => {
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => {
+    console.error(err);
+    alert('Allow the app to know your whereabouts! 😁');
+  }
+);
