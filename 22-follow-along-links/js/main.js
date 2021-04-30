039bdf19f8f461452660dc38fabe16adce6db063
2y2son4 <adrianadelteso@gmail.com>
2021-04-30 11:52:29 +0200

    :art: Implement basic highLight function

--- 22-follow-along-links/js/main.js
+++ 22-follow-along-links/js/main.js
@@ -8,9 +8,17 @@ document.body.append(highlight);
 
 function highlighLink() {
   const linkCoords = this.getBoundingClientRect();
-  highlight.style.width = `${linkCoords.width}px`;
-  highlight.style.height = `${linkCoords.height}px`;
-  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX,
+  };
+
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 
 triggers.forEach((a) => a.addEventListener('mouseenter', highlighLink));
