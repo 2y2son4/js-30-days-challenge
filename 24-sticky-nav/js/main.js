6e8d0d397312237d849e8366503606d15a6f6331
2y2son4 <adrianadelteso@gmail.com>
2021-04-30 18:43:37 +0200

    :sparkles: Add main functionality for nav

--- 24-sticky-nav/js/main.js
+++ 24-sticky-nav/js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+  if (window.scrollY >= topOfNava) {
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+window.addEventListener('scroll', fixNav);
