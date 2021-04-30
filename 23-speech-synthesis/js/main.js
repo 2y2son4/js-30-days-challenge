aa6e2cf81112760c4228277445810a7b19a3eaeb
2y2son4 <adrianadelteso@gmail.com>
2021-04-30 16:34:43 +0200

    :sparkles: Add voices for main functionality

--- 23-speech-synthesis/js/main.js
+++ 23-speech-synthesis/js/main.js
@@ -6,3 +6,14 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map((voice) => `<option value="${voice.name}"> ${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
