fdf3b7be8575ba7e162a931ebc938662c076f40a
2y2son4 <adrianadelteso@gmail.com>
2021-04-30 11:44:30 +0200

    :sparkles: Add basic highLight function

--- 22-follow-along-links/js/main.js
+++ 22-follow-along-links/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const triggers = document.querySelectorAll('a');
+
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlighLink() {
+  const linkCoords = this.getBoundingClientRect();
+  highlight.style.width = `${linkCoords.width}px`;
+  highlight.style.height = `${linkCoords.height}px`;
+}
+
+triggers.forEach((a) => a.addEventListener('mouseenter', highlighLink));
