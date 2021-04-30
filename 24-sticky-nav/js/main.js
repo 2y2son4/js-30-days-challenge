4a70b6cd83c5821e5f241bdd93e75ea48fe22ca3
2y2son4 <adrianadelteso@gmail.com>
2021-04-30 18:54:35 +0200

    :sparkles: Fix main functionality and styles

--- 24-sticky-nav/js/main.js
+++ 24-sticky-nav/js/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const nav = document.querySelector('#main');
-const topOfNav = nav.offsetTop;
+let topOfNav = nav.offsetTop;
 
 function fixNav() {
-  if (window.scrollY >= topOfNava) {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
     document.body.classList.add('fixed-nav');
   } else {
     document.body.classList.remove('fixed-nav');
+    document.body.style.paddingTop = 0;
   }
 }
 
