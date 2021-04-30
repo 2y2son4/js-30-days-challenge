9b3c45f55e3dcc9b1c4d76bf22008c8299ba052c
2y2son4 <adrianadelteso@gmail.com>
2021-04-30 16:22:51 +0200

    :sparkles: Add querySelector for main functionality

--- 23-speech-synthesis/js/main.js
+++ 23-speech-synthesis/js/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
