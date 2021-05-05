ed5d72f76ac5bb518ef961a87d40472dd1f47892
2y2son4 <adrianadelteso@gmail.com>
2021-05-05 11:05:13 +0200

    :sparkles: Add main events function

--- 25-capture-propagation-bubbling/js/main.js
+++ 25-capture-propagation-bubbling/js/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
+function logText(ev) {
+  console.log(this.classList.value);
+
+  // stop bubbling
+  ev.stopPropagation();
+}
+
+divs.forEach((div) =>
+  div.addEventListener('click', logText, {
+    cature: false,
+    // it listen to a click once and the stops
+    once: true,
+  })
+);
+
+button.addEventListener(
+  'click',
+  () => {
+    console.log('CLICK, MF!');
+  },
+  {
+    // this unbind the event and stops it from happening more than once
+    once: true,
+  }
+);
