79d723c9201b5f58bcfa87bc8e4e846f593dc3be
2y2son4 <adrianadelteso@gmail.com>
2021-05-05 13:11:56 +0200

    :sparkles: Add main function for dropdowns

--- 26-follow-along-dropdown/js/main.js
+++ 26-follow-along-dropdown/js/main.js
@@ -2,14 +2,37 @@
 
 const triggers = document.querySelectorAll('.cool > li');
 const background = document.querySelector('.dropdownBackground');
-const navElement = document.querySelector('.top');
+const nav = document.querySelector('.top');
 
 function handleEnter() {
-  console.log('enter');
+  this.classList.add('trigger-enter');
+  setTimeout(
+    () =>
+      // with an arrow function the value of this is inherit from the parent, it doesn't happen with normal fuctions
+      this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'),
+    150
+  );
+  background.classList.add('open');
+
+  const dropdown = this.querySelector('.dropdown');
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  const navCoords = nav.getBoundingClientRect();
+
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left,
+  };
+
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
+  background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
 }
 
 function handleLeave() {
-  console.log('leave');
+  this.classList.remove('trigger-enter', 'trigger-entered-active');
+  background.classList.remove('open');
 }
 
 triggers.forEach((trigger) => trigger.addEventListener('mouseenter', handleEnter));
