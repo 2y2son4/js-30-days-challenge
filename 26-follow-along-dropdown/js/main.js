52c52f3ebbfb562a96d077df5b0e45b224c3d493
2y2son4 <adrianadelteso@gmail.com>
2021-05-05 11:50:32 +0200

    :bug: Fix links

--- 26-follow-along-dropdown/js/main.js
+++ 26-follow-along-dropdown/js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const navElement = document.querySelector('.top');
+
+function handleEnter() {
+  console.log('enter');
+}
+
+function handleLeave() {
+  console.log('leave');
+}
+
+triggers.forEach((trigger) => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach((trigger) => trigger.addEventListener('mouseleave', handleLeave));
