fa0b27776a5e060bad4c027f5b25eb9fa8089c40
2y2son4 <adrianadelteso@gmail.com>
2021-05-06 11:16:06 +0200

    :sparkles: Add main scroll function

--- 27-click-and-drag/js/main.js
+++ 27-click-and-drag/js/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (ev) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = ev.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (ev) => {
+  if (!isDown) return; // stops the function from running
+  ev.preventDefault();
+  const x = ev.pageX - slider.offsetLeft;
+  //   console.log({ x, startX });
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+});
