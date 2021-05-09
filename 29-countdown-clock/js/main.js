43c6f5db49bb0dff588db9fd6eb0303066548c7d
2y2son4 <adrianadelteso@gmail.com>
2021-05-09 14:28:11 +0200

    :sparkles: Add main function

--- 29-countdown-clock/js/main.js
+++ 29-countdown-clock/js/main.js
@@ -1,1 +1,29 @@
 'use strict';
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Datenow()) / 1000);
+    // check if we should stop it
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+    // display it
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  console.log(seconds);
+}
