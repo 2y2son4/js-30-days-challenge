0766655d6c8b9bd0c9c83a72893f80b4e16206d8
2y2son4 <adrianadelteso@gmail.com>
2021-05-09 19:06:24 +0200

    :sparkles: Add whack function

--- 30-whack-a-mole-game/js/main.js
+++ 30-whack-a-mole-game/js/main.js
@@ -4,16 +4,18 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
+let score = 0;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
 function randomHole(holes) {
-  const idx = Math.floor(math.random() * holes.length);
+  const idx = Math.floor(Math.random() * holes.length);
   const hole = holes[idx];
   if (hole === lastHole) {
-    return ramdomHole(holes);
+    return randomHole(holes);
   }
 
   lastHole = hole;
@@ -25,4 +27,26 @@ function peep() {
   const hole = randomHole(holes);
 
   hole.classList.add('up');
+
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) peep();
+  }, time);
 }
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  score = 0;
+  timeUp = false;
+  peep();
+  setTimeout(() => (timeUp = true), 10000);
+}
+
+function bonk(ev) {
+  if (!ev.isTrusted) return;
+  score++;
+  this.classList.remove('up');
+  scoreBoard.textContent = score;
+}
+
+moles.forEach((mole) => mole.addEventListener('click', bonk));
