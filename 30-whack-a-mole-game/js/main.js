791feee5c89ee56d0f92744a7319563c98c6f379
2y2son4 <adrianadelteso@gmail.com>
2021-05-09 18:47:53 +0200

    :sparkles: Add main functions

--- 30-whack-a-mole-game/js/main.js
+++ 30-whack-a-mole-game/js/main.js
@@ -3,3 +3,26 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    return ramdomHole(holes);
+  }
+
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+
+  hole.classList.add('up');
+}
